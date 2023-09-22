4c99cb15eac365572ace808e00556f3cca6b3b71
ya_katyara <ya_katyara@mail.ru>
2023-09-22 14:15:50 +0300

    Changes for next export

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -41,7 +41,7 @@ const Product = motion(forwardRef(({ product, className, ...props }: ProductProp
     <div className={className} {...props} ref={ref}>
         <Card className={styles.product}>
             <div className={styles.logo}>
-                <Image 
+                <img 
                     src={process.env.NEXT_PUBLIC_DOMAIN + product.image} 
                     alt={product.title}
                     width={70}
