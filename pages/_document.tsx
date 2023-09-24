5c47cfd6bd61727545e29e69ac84da7be84cef2b
ya_katyara <ya_katyara@mail.ru>
2023-09-24 22:43:27 +0300

    fix build error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
 				<Head>
 					<link rel="icon" href="/favicon.ico" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
 					{/* <Script src='' strategy="beforeInteractive" /> */}
 				</Head>
